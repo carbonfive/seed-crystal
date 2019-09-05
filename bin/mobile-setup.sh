dc26d4fe418f6deafd598911128d0afa210e2995
Ken Mayer <ken.mayer@carbonfive.com>
2019-09-05 16:35:48 -0700

    de-linting

--- bin/mobile-setup.sh
+++ bin/mobile-setup.sh
@@ -10,7 +10,11 @@ cache_root_password
 
 brew bundle --file=Brewfile.mobile
 
+# shellcheck disable=SC2016
 add_line_to_file 'export ANDROID_SDK_ROOT="$HOME/Library/Android/sdk"' ~/.bash_profile
+# shellcheck disable=SC2016
 add_line_to_file 'export ANDROID_HOME="$HOME/Library/Android/sdk"' ~/.bash_profile
+# shellcheck disable=SC2016
 add_line_to_file 'export JAVA_HOME=/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home' ~/.bash_profile
+# shellcheck disable=SC2016
 add_line_to_file 'export PATH="$ANDROID_SDK_ROOT/platform-tools:$PATH"' ~/.bash_profile
