313756ec2a09d9612ed62cbbebedf69a6b3fced9
Ken Mayer <ken.mayer@carbonfive.com>
2019-09-15 16:31:05 -0700

    Add some docs to ~/.bash_profile

--- bin/java-setup.sh
+++ bin/java-setup.sh
@@ -8,8 +8,8 @@ setup_color
 
 cache_root_password
 
-brew bundle --file=Brewfile.java
-
-# https://www.mkyong.com/java/how-to-set-java_home-environment-variable-on-mac-os-x/
+add_line_to_file '# https://www.mkyong.com/java/how-to-set-java_home-environment-variable-on-mac-os-x/' ~/.bash_profile
 # shellcheck disable=SC2016
 add_line_to_file 'export JAVA_HOME=$(/usr/libexec/java_home)' ~/.bash_profile
+
+brew bundle --file=Brewfile.java
